7fc17d33dad8f7cc54d907ec585df70c9691329b
Sumaiya-topu <topusumaiya@gmail.com>
2023-09-15 09:44:55 +0600

    small changes in routes

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -4,6 +4,7 @@ import ipix from "../../assets/go-tutor.png";
 import gotutor from "../../assets/apple-touch-icon.png";
 import code_camp from "../../assets/logo.png";
 import "./Projects.css";
+import { Link } from "react-router-dom";
 
 const Projects = () => {
   const projects = [
@@ -49,21 +50,24 @@ const Projects = () => {
         className=" grid grid-cols-1 lg:grid-cols-3 w-3/4 mx-auto my-32"
       >
         {projects.map((project) => (
-          <div className=" hover:-translate-y-1 hover:scale-125 duration-300 lg:mt-20">
-            <div className="flex justify-center">
-              <div className=" bg-black w-52 h-52 border rounded-full">
-                <img src={project.img} className=" w-full p-10 " alt="" />
+          <Link to={project.live_link}>
+            {" "}
+            <div className=" hover:-translate-y-1 hover:scale-125 duration-300 lg:mt-20">
+              <div className="flex justify-center">
+                <div className=" bg-black w-52 h-52 border rounded-full">
+                  <img src={project.img} className=" w-full p-10 " alt="" />
+                </div>
               </div>
+              <div className=" lg:p-10 ">
+                <a href={project.live_link}>
+                  <h1 className="text-2xl font-bold text-orange-400 text-center hover:text-rose-500">
+                    {project.title}
+                  </h1>
+                </a>
+              </div>
+              <div className="divider"></div>
             </div>
-            <div className=" lg:p-10 ">
-              <a href={project.live_link}>
-                <h1 className="text-2xl font-bold text-orange-400 text-center hover:text-rose-500">
-                  {project.title}
-                </h1>
-              </a>
-            </div>
-            <div className="divider"></div>
-          </div>
+          </Link>
         ))}
       </div>
     </div>
