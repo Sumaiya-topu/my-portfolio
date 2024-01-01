7761e5cce287cec9e6dbdc1ebaa01afe020273ed
Sumaiya-topu <topusumaiya@gmail.com>
2024-01-02 03:32:39 +0600

    updating skill section

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,10 @@ module.exports = withMT({
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
+    color:{
+      gray: "#1c1c1c",
+      lightGray:"#888499"
+    }
   },
   plugins: [require("daisyui")],
 });
